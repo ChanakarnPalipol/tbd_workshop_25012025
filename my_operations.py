0b3a5d4afaa15365eba22fabb62d1818984ae766
2pleum <rachatacnx13@gmail.com>
2025-01-25 14:00:52 +0700

    B edit done

--- my_operations.py
+++ my_operations.py
@@ -9,9 +9,8 @@ def add(num1: int, num2: int) -> None:
 # Member B: edit this function.
 def diff(num1: int, num2: int) -> None:
     # Edit code under this line
-
+    res = num1 - num2 
     print(num1, "-", num2, "is", res)
-    pass
 
 
 # Member C: edit this function.
